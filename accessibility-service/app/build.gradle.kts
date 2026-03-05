06f0eacfb66e2b42769411fc8f91929b2e8bf285
vancheszz <vancheszzgh@gmail.com>
2026-03-05 03:03:57 +0300

    Many Graddle Fixes and Realize protobuf methods

--- accessibility-service/app/build.gradle.kts
+++ accessibility-service/app/build.gradle.kts
@@ -1,25 +1,31 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     alias(libs.plugins.android.application)
-    // Добавляем плагин
+    alias(libs.plugins.protobuf)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.project.ratatoskr"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.project.ratatoskr"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDir("src/main/java")
+            // The protobuf plugin will automatically find proto files in src/main/proto
+        }
     }
 
     buildTypes {
@@ -31,19 +37,44 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+}
 
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                create("java") {
+                    option("lite")
+                }
+                create("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.protobuf.kotlin.lite)
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
 }
--- accessibility-service/app/src/main/java/com/project/ratatoskr/RatatoskrService.kt
+++ accessibility-service/app/src/main/java/com/project/ratatoskr/RatatoskrService.kt
@@ -5,27 +5,117 @@ import android.accessibilityservice.AccessibilityService
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
+import java.io.OutputStream
+import java.net.Socket
+import java.util.concurrent.Executors
+
+
 class RatatoskrService: AccessibilityService() {
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        val eventType = AccessibilityEvent.eventTypeToString(event?.eventType ?: 0 )
-        Log.d("RATATOSKR", "Event Type $eventType")
-        val source: AccessibilityNodeInfo? = event?.source
-        if(source != null){
-            val viewId = source.viewIdResourceName ?: "no-id"
-            val sourceText = source.text ?: "no-text"
-            val clsName = source.className ?: "unknown-class"
 
-            Log.d("RATATOSKR", "Element: $clsName ID: $viewId Text: $sourceText" )
+    private val executor = Executors.newSingleThreadExecutor()
+    private var outputStream : OutputStream? = null
+    private var socket: Socket ?= null
+    private var isConnecting = false
 
-            //for API Level < 34
-            source.recycle()
+
+    private fun connectToAgent(){
+        if (isConnecting) return
+        isConnecting = true
+
+        executor.execute {
+            //try to connnect
+            try {
+                socket = Socket("127.0.0.1", 9999)
+                outputStream = socket?.getOutputStream()
+                Log.d("RATATOSKR", "Connect to Agent Successfully")
+            }catch (e: Exception){
+                Log.e("RATATOSKR", "Fail Connection")
+                Thread.sleep(5000)
+                //recursive connection
+                connectToAgent()
+            }
         }
+    }
+    private fun captureScreen(rootNode: AccessibilityNodeInfo): ScreenDump{
+        val dumpBuilder = ScreenDump.newBuilder()
+            .setPackageName(rootNode.packageName?.toString() ?: "unknown")
+            .setTimestamp(System.currentTimeMillis())
+            // Здесь можно добавить реальные размеры экрана из WindowManager
+            .setWidth(1080)
+            .setHeight(1920)
+
+        flattenTree(rootNode, dumpBuilder)
+        return dumpBuilder.build()
+
+    }
+    private fun flattenTree(node: AccessibilityNodeInfo, dumpBuilder: ScreenDump.Builder) {
+        // collect data if have ID
+        val text = node.text?.toString() ?: ""
+        val resId = node.viewIdResourceName ?: ""
+
+        if (text.isNotEmpty() || resId.isNotEmpty() || node.isClickable) {
+            val bounds = android.graphics.Rect()
+            node.getBoundsInScreen(bounds)
+
+            val nodeProto = UiNode.newBuilder()
+                .setText(text)
+                .setResourceId(resId)
+                .setClassName(node.className?.toString() ?: "")
+                .setIsClickable(node.isClickable)
+                .setBounds(Rect.newBuilder()
+                    .setLeft(bounds.left)
+                    .setTop(bounds.top)
+                    .setRight(bounds.right)
+                    .setBottom(bounds.bottom))
+                .build()
+
+            dumpBuilder.addNodes(nodeProto)
+        }
+
+        for (i in 0 until node.childCount) {
+            node.getChild(i)?.let {
+                flattenTree(it, dumpBuilder)
+                it.recycle()
+            }
+        }
+    }
+    private fun SendDump(dump: ScreenDump){
+            try{
+               outputStream?.let{ stream ->
+                   val bytes = dump.toByteArray()
+                   val size = bytes.size
+                   stream.write(byteArrayOf(
+                       (size shr 24 ).toByte(),
+                       (size shr 16).toByte(),
+                       (size shr 8).toByte(),
+                       size.toByte()
+                   ))
+                   stream.write(bytes)
+                   stream.flush()
+               }
+            }catch (e: Exception){
+                Log.e("RATATOSKR", "Sending Error: ${e.message}")
+                connectToAgent() // try to recconect
+            }
+    }
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+       if(event?.eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED || event?.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED){
+           val rootNode = rootInActiveWindow ?: return
+
+           executor.execute{
+               val dump = captureScreen(rootNode)
+               SendDump(dump)
+               rootNode.recycle()
+           }
+
+       }
 
     }
 
     override fun onServiceConnected() {
         super.onServiceConnected()
         Log.d("RATATOSKR", "Service is Running")
+        connectToAgent()
     }
 
     override fun onInterrupt() {
